32970acd842e11dc593359887877d1129c80d74d
Chris-Humphrey <clhumphrey21@gmail.com>
2019-07-24 18:37:49 -0500

    github

--- src/index.js
+++ src/index.js
@@ -2,11 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Welcome from './components/Welcome';
+import Feature from './components/Feature';
+import Signin from './components/Signin';
+import Signout from './components/Signout';
+import Signup from './components/Signup';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(()=>{});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    
+    <Provider store={store}>
+        <BrowserRouter>
+            <App>
+                <Route exact path='/' component={Welcome}/>
+                <Route path='/feature' component={Feature}/>
+                <Route path='/signin' component={Signin}/>
+                <Route path='/signup' component={Signup}/>
+                <Route path='/signout' component={Signout}/>
+            </App>
+        </BrowserRouter>
+    </Provider>
+
+    , document.getElementById('root'));
+
